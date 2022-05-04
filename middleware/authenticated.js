7e39915e3794379ac7531e32892a66895f831468
ken3986 <naka_ken8686@yahoo.co.jp>
2022-05-04 15:16:33 +0900

    functionsにアクセスできるかテスト

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -12,39 +12,41 @@ export default async ({ store, redirect, route, $axios, app }) => {
   }
 
   console.log(app.$cookies.get('teten-portfolio'))
-
+console.log('authenticated')
+const hello = await $axios.get('/.netlify/functions/hello')
+console.log(hello)
   // 認証パスを持っている場合
-  if (app.$cookies.get('teten-portfolio')) {
-    const response = await $axios.post('/.netlify/functions/authentication', {
-      hashedPassword: app.$cookies.get('teten-portfolio')
-    })
-    if (response.status == 200) {
-      store.commit('login')
+  // if (app.$cookies.get('teten-portfolio')) {
+  //   const response = await $axios.post('/.netlify/functions/authentication', {
+  //     hashedPassword: app.$cookies.get('teten-portfolio')
+  //   })
+  //   if (response.status == 200) {
+  //     store.commit('login')
 
-      if (route.path === '/login') {
-        return redirect({
-          path: '/'
-        })
-      }
-      return false
-    }
-  }
+  //     if (route.path === '/login') {
+  //       return redirect({
+  //         path: '/'
+  //       })
+  //     }
+  //     return false
+  //   }
+  // }
 
-  // ログインしておらず、認証パスを持っていない場合
+  // // ログインしておらず、認証パスを持っていない場合
 
-    // ログインページにいる場合
-    if (route.path === '/login') {
-      return false
-    }
+  //   // ログインページにいる場合
+  //   if (route.path === '/login') {
+  //     return false
+  //   }
 
-    // ログインページ以外にいる場合
-    return redirect({
-      path: '/login',
-      query: {
-        showMessage: true,
-        redirect: route.path,
-      }
-    })
+  //   // ログインページ以外にいる場合
+  //   return redirect({
+  //     path: '/login',
+  //     query: {
+  //       showMessage: true,
+  //       redirect: route.path,
+  //     }
+  //   })
 
 
 }
