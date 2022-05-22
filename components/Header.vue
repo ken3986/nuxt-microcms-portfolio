5e7faf0f55c6db2b9370f0dbc56d8bf129b893f3
ken3986 <naka_ken8686@yahoo.co.jp>
2022-05-22 14:58:07 +0900

    ヘッダーをcontainer内に

--- components/Header.vue
+++ components/Header.vue
@@ -1,34 +1,37 @@
 <template>
   <header class="header">
+
     <b-navbar toggleable="lg" type="dark" variant="add">
-      <b-navbar-brand :to="`/`" class="logo">{{ title }}</b-navbar-brand>
+        <b-container>
+          <b-navbar-brand :to="`/`" class="logo">{{ title }}</b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="header-menu">
-          <b-nav-item
-            :to="`/works`"
-            class="header-menu-link"
-          >Works</b-nav-item>
-          <b-nav-item
-            :to="`/about`"
-            class="header-menu-link"
-          >About</b-nav-item>
-        </b-navbar-nav>
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="header-menu">
+              <b-nav-item
+                :to="`/works`"
+                class="header-menu-link"
+              >Works</b-nav-item>
+              <b-nav-item
+                :to="`/about`"
+                class="header-menu-link"
+              >About</b-nav-item>
+            </b-navbar-nav>
 
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <!-- <button @click.prevent="logout" v-show="$store.state.loggedIn">ログアウト</button> -->
-          <!-- <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown> -->
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+              <!-- <button @click.prevent="logout" v-show="$store.state.loggedIn">ログアウト</button> -->
+              <!-- <b-nav-item-dropdown text="Lang" right>
+                <b-dropdown-item href="#">EN</b-dropdown-item>
+                <b-dropdown-item href="#">ES</b-dropdown-item>
+                <b-dropdown-item href="#">RU</b-dropdown-item>
+                <b-dropdown-item href="#">FA</b-dropdown-item>
+              </b-nav-item-dropdown> -->
+            </b-navbar-nav>
+          </b-collapse>
+        </b-container>
+      </b-navbar>
   </header>
 </template>
 
